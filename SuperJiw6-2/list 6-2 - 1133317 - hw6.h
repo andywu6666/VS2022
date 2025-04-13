8e0d2467488465a3be98b81b440e536f452d4268
andywu6666 <andywu0010@gmail.com>
2025-04-13 18:44:24 +0800

    F_SuperJiw6

--- SuperJiw6-2/list 6-2 - 1133317 - hw6.h
+++ SuperJiw6-2/list 6-2 - 1133317 - hw6.h
@@ -6,9 +6,9 @@
 template< typename ValueType >
 struct ListNode // list node
 {
-   ListNode *next;  // successor node, or first element if head
-   ListNode *prev;  // predecessor node, or last element if head
-   ValueType myVal; // the stored value, unused if head
+    ListNode* next;  // successor node, or first element if head
+    ListNode* prev;  // predecessor node, or last element if head
+    ValueType myVal; // the stored value, unused if head
 };
 
 
@@ -17,25 +17,25 @@ template< typename Ty >
 class ListVal
 {
 public:
-   using node = ListNode< Ty >;
-   using nodePtr = node *;
+    using node = ListNode< Ty >;
+    using nodePtr = node*;
 
-   using value_type = Ty;
-   using size_type = size_t;
-   using difference_type = ptrdiff_t;
-   using pointer = value_type *;
-   using const_pointer = const value_type *;
-   using reference = value_type &;
-   using const_reference = const value_type &;
+    using value_type = Ty;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using reference = value_type&;
+    using const_reference = const value_type&;
 
-   ListVal() // initialize data
-      : myHead(),
-        mySize( 0 )
-   {
-   }
+    ListVal() // initialize data
+        : myHead(),
+        mySize(0)
+    {
+    }
 
-   nodePtr myHead; // pointer to head node
-   size_type mySize; // number of elements
+    nodePtr myHead; // pointer to head node
+    size_type mySize; // number of elements
 };
 
 
@@ -43,145 +43,196 @@ public:
 template< typename Ty >
 class list // bidirectional linked list
 {
-   using node = ListNode< Ty >;
-   using nodePtr = node *;
-   using ScaryVal = ListVal< Ty >;
+    using node = ListNode< Ty >;
+    using nodePtr = node*;
+    using ScaryVal = ListVal< Ty >;
 
 public:
-   using value_type = Ty;
-   using size_type = size_t;
-   using difference_type = ptrdiff_t;
-   using pointer = value_type *;
-   using const_pointer = const value_type *;
-   using reference = value_type &;
-   using const_reference = const value_type &;
+    using value_type = Ty;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using reference = value_type&;
+    using const_reference = const value_type&;
 
-   using iterator = node *;
-   using const_iterator = const node *;
+    using iterator = node*;
+    using const_iterator = const node*;
 
-   // empty container constructor (default constructor)
-   // Constructs an empty container, with no elements.
-   list()
-      : myData()
-   {
-      myData.myHead = new node;
-      myData.myHead->myVal = Ty();
-      myData.myHead->prev = myData.myHead->next = myData.myHead;
-   }
+    // empty container constructor (default constructor)
+    // Constructs an empty container, with no elements.
+    list()
+        : myData()
+    {
+        myData.myHead = new node;
+        myData.myHead->myVal = Ty();
+        myData.myHead->prev = myData.myHead->next = myData.myHead;
+    }
 
-   list( size_type count ) // construct list from count * Ty()
-      : myData()
-   {
-      myData.myHead = new node;
-      myData.myHead->myVal = Ty();
-      myData.myHead->prev = myData.myHead->next = myData.myHead;
+    list(size_type count) // construct list from count * Ty()
+        : myData()
+    {
+        myData.myHead = new node;
+        myData.myHead->myVal = Ty();
+        myData.myHead->prev = myData.myHead->next = myData.myHead;
 
+        for (size_type i = 0; i < count; ++i)
+        {
+            nodePtr newNode = new node;
+            newNode->myVal = Ty();
 
+            newNode->prev = myData.myHead->prev;
+            newNode->next = myData.myHead;
+            myData.myHead->prev->next = newNode;
+            myData.myHead->prev = newNode;
 
-
-
-   }
-
-   // List destructor
-   // Destroys the container object.
-   // Deallocates all the storage capacity allocated by the list container.
-   ~list()
-   {
-      clear();
-      delete myData.myHead;
-   }
-
-   // Assigns new contents to the container, replacing its current contents,
-   // and modifying its size accordingly.
-   // Copies all the elements from "right" into the container
-   // (with "right" preserving its contents).
-   list& operator=( const list &right )
-   {
-      if( this != &right )
-      {
-         if( right.myData.mySize == 0 ) // the right list is empty
-         {
-            if( myData.mySize != 0 ) // the left list is not empty
-               clear();
-         }
-         else // the right list is not empty
-         {
+            myData.mySize++;
+        }
 
 
 
 
+    }
 
-         }
-      }
+    // List destructor
+    // Destroys the container object.
+    // Deallocates all the storage capacity allocated by the list container.
+    ~list()
+    {
+        clear();
+        delete myData.myHead;
+    }
 
-      return *this;
-   }
+    // Assigns new contents to the container, replacing its current contents,
+    // and modifying its size accordingly.
+    // Copies all the elements from "right" into the container
+    // (with "right" preserving its contents).
+    list& operator=(const list& right)
+    {
+        if (this != &right)
+        {
+            if (right.myData.mySize == 0) // the right list is empty
+            {
+                if (myData.mySize != 0) // the left list is not empty
+                    clear();
+            }
+            else // the right list is not empty
+            {
+                iterator leftIt = this->begin();
+                const_iterator rightIt = right.begin();
 
-   // Returns an iterator pointing to the first element in the list container.
-   // If the container is empty, the returned iterator value shall not be dereferenced.
-   iterator begin()
-   {
-      return iterator( myData.myHead->next );
-   }
+                while (leftIt != this->end() && rightIt != right.end())
+                {
+                    leftIt->myVal = rightIt->myVal;
+                    leftIt = leftIt->next;
+                    rightIt = rightIt->next;
+                }
 
-   // Returns an iterator pointing to the first element in the list container.
-   // If the container is empty, the returned iterator value shall not be dereferenced.
-   const_iterator begin() const
-   {
-      return const_iterator( myData.myHead->next );
-   }
+                if (rightIt != right.end())
+                {
+                    while (rightIt != right.end())
+                    {
+                        nodePtr newNode = new node;
+                        newNode->myVal = rightIt->myVal;
 
-   // Returns an iterator referring to the past-the-end element in the list container.
-   // The past-the-end element is the theoretical element
-   // that would follow the last element in the list container.
-   // It does not point to any element, and thus shall not be dereferenced.
-   // If the container is empty, this function returns the same as list::begin.
-   iterator end()
-   {
-      return iterator( myData.myHead );
-   }
+                        newNode->prev = myData.myHead->prev;
+                        newNode->next = myData.myHead;
+                        myData.myHead->prev->next = newNode;
+                        myData.myHead->prev = newNode;
 
-   // Returns an iterator referring to the past-the-end element in the list container.
-   // The past-the-end element is the theoretical element
-   // that would follow the last element in the list container.
-   // It does not point to any element, and thus shall not be dereferenced.
-   // If the container is empty, this function returns the same as list::begin.
-   const_iterator end() const
-   {
-      return const_iterator( myData.myHead );
-   }
+                        rightIt = rightIt->next;
+                    }
+                }
 
-   // Returns the number of elements in the list container.
-   size_type size() const
-   {
-      return myData.mySize;
-   }
+                else if (leftIt != this->end())
+                {
+                    nodePtr lastNodeToKeep = leftIt->prev;
 
-   // Returns whether the list container is empty (i.e. whether its size is 0).
-   bool empty() const
-   {
-      return myData.mySize == 0;
-   }
+                    while (leftIt != this->end())
+                    {
+                        nodePtr nodeToDelete = leftIt;
+                        leftIt = leftIt->next;
+                        delete nodeToDelete;
+                    }
 
-   // Removes all elements from the list container (which are destroyed),
-   // and leaving the container with a size of 0.
-   void clear() // erase all
-   {
-      if( myData.mySize != 0 ) // the list is not empty
-      {
-         while( myData.myHead->next != myData.myHead )
-         {
-            myData.myHead->next = myData.myHead->next->next;
-            delete myData.myHead->next->prev;
-         }
+                    lastNodeToKeep->next = myData.myHead;
+                    myData.myHead->prev = lastNodeToKeep;
+                }
 
-         myData.myHead->prev = myData.myHead;
-         myData.mySize = 0;
-      }
-   }
+                myData.mySize = right.myData.mySize;
+
+
+            }
+        }
+
+        return *this;
+    }
+
+    // Returns an iterator pointing to the first element in the list container.
+    // If the container is empty, the returned iterator value shall not be dereferenced.
+    iterator begin()
+    {
+        return iterator(myData.myHead->next);
+    }
+
+    // Returns an iterator pointing to the first element in the list container.
+    // If the container is empty, the returned iterator value shall not be dereferenced.
+    const_iterator begin() const
+    {
+        return const_iterator(myData.myHead->next);
+    }
+
+    // Returns an iterator referring to the past-the-end element in the list container.
+    // The past-the-end element is the theoretical element
+    // that would follow the last element in the list container.
+    // It does not point to any element, and thus shall not be dereferenced.
+    // If the container is empty, this function returns the same as list::begin.
+    iterator end()
+    {
+        return iterator(myData.myHead);
+    }
+
+    // Returns an iterator referring to the past-the-end element in the list container.
+    // The past-the-end element is the theoretical element
+    // that would follow the last element in the list container.
+    // It does not point to any element, and thus shall not be dereferenced.
+    // If the container is empty, this function returns the same as list::begin.
+    const_iterator end() const
+    {
+        return const_iterator(myData.myHead);
+    }
+
+    // Returns the number of elements in the list container.
+    size_type size() const
+    {
+        return myData.mySize;
+    }
+
+    // Returns whether the list container is empty (i.e. whether its size is 0).
+    bool empty() const
+    {
+        return myData.mySize == 0;
+    }
+
+    // Removes all elements from the list container (which are destroyed),
+    // and leaving the container with a size of 0.
+    void clear() // erase all
+    {
+        if (myData.mySize != 0) // the list is not empty
+        {
+            while (myData.myHead->next != myData.myHead)
+            {
+                myData.myHead->next = myData.myHead->next->next;
+                delete myData.myHead->next->prev;
+            }
+
+            myData.myHead->prev = myData.myHead;
+            myData.mySize = 0;
+        }
+    }
 
 private:
-   ScaryVal myData;
+    ScaryVal myData;
 };
 
 #endif // LIST
