9c7aa9894eb189340c35ea8bfa56a0f30ab42d74
andywu6666 <andywu0010@gmail.com>
2025-05-27 21:15:15 +0800

    Revised MakingOrder.cpp

--- SuperJiwFinal/MakingOrder.cpp
+++ SuperJiwFinal/MakingOrder.cpp
@@ -12,28 +12,40 @@ MakingOrder::MakingOrder( string theEmail, AccountDatabase &theAccountDatabase )
 
 void MakingOrder::run()
 {
-    Account user;
-    user.setCart(0);
-    int foodCode = 0;
-    int quantity = 0;
+    int newCart[14], * oldCart = accountDatabase.getCart(email); //add
+    Account user(email); //modify
+    user.setCart(newCart); //modify
+
+   
+    int foodCode = -1; //modify
+    int quantity = newCart[foodCode]; //modify
 
     //showing Cart
     user.displayCart();
 
-    cout << "\nEnter foods code  (0 to end): ";
-    
-    while ((foodCode = inputAnInteger(0, 13)) == -1)
+   
+    if (oldCart != nullptr) //add
     {
+        for (int i = 1; i < 14; i++) { //add
+            newCart[i] = (oldCart[i] >= 0 ? oldCart[i] : 0); //add
+        }
+    }
+    do { //add
+        cout << "\nEnter foods code  (0 to end): ";
+
+    } while ((foodCode = inputAnInteger(0, 13)) == -1); //modify
+    
         if (foodCode == 0)
             return;
-    }//unsure
+    //unsure
 
     cout << "\nEnter quantity: ";
     cin >> quantity;
+    cin.ignore(); //add
 
-    bool backToTop = false;
-    while (!backToTop)
-    {
+    bool backToTop = false; 
+    while (!backToTop) 
+   {
         cout << "\n1. Continue Shopping\n"
             << "2. Save your Shopping Cart\n"
             << "3. Abandon\n";
@@ -42,23 +54,28 @@ void MakingOrder::run()
         do cout << "Enter your choice: (1~3): ";
         while ((choice = inputAnInteger(1, 3)) == -1);
 
+        user.setCart(newCart); //add
+
         switch (choice)
         {
         case 1:
+            accountDatabase.modifyCart(user); //add
             run();
+            backToTop = true; //add
             break;
         case 2:
-            user.modifyCart(foodCode, quantity);
+            accountDatabase.modifyCart(user); //modify
             // call function to save shopping cart
-            backToTop = true;
-            break;
+            backToTop = true; 
+            break; 
         case 3:
-            backToTop = true; // this session should end
+            backToTop = true; // this session should end 
+            break; //add
         }
-    }
+   }
 
 
-    accountDatabase.displayOrders(email);
+  //  accountDatabase.displayOrders(email); //delete
 
 
 
